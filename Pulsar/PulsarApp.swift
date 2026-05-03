80c1c7f3b3781b02ef013f97eaa88a1cca526245
BenjaGood <68666564+BenjaGood@users.noreply.github.com>
2026-05-03 15:49:01 -0600

    Initial Commit

---
+++ Pulsar/PulsarApp.swift
@@ -0,0 +1,17 @@
+//
+//  PulsarApp.swift
+//  Pulsar
+//
+//  Created by Benjamín Gutierrez Mendoza on 03/05/26.
+//
+
+import SwiftUI
+
+@main
+struct PulsarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
